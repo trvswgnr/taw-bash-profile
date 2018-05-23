93d58db2043ac8e07fff9864cbb94ea1be4fd6f0
trav <trvswgnr@gmail.com>
2018-05-23 17:38:23 -0400

    ...

--- _system-ops.sh
+++ _system-ops.sh
@@ -12,4 +12,3 @@ alias hideHidden='defaults write com.apple.finder ShowAllFiles FALSE'
  
 # Clean up LaunchServices to remove duplicates in the "Open With" menu
 alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
- 
