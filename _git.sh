f133d5b9a21cde8a47e34395d22acf8fc83e5da4
trav <trvswgnr@gmail.com>
2018-05-23 17:07:29 -0400

    rearranged

--- _git.sh
+++ _git.sh
@@ -1,5 +1,7 @@
 # -- GIT -- #
 
+source ~/bash_profile/_add-repo.sh
+
 alias git-change='git checkout'
 alias cbranch='git checkout'
 alias st="git status"
@@ -9,5 +11,3 @@ gcaa() { git add --all && git commit -m "$*" ; } # add and commit all files
 # use custom nano
 alias my="PATH=/usr/local/bin:$PATH"
 alias nano="my nano"
-
-source ~/bash_profile/_add-repo.sh
