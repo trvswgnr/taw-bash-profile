c54e22383ecc8eb24e23c436199306066d228995
Travis Wagner <travis@Travis-MBP.local>
2018-06-01 11:48:19 -0400

    removed duplicate pm aliases

--- bash_profile.sh
+++ bash_profile.sh
@@ -22,11 +22,3 @@ alias pmg="pm go"
 alias pmrm="pm remove"
 alias pml="pm list"
 # end PM
-
-# PM functions
-source ~/.pm/pm.bash
-alias pma="pm add"
-alias pmg="pm go"
-alias pmrm="pm remove"
-alias pml="pm list"
-# end PM
