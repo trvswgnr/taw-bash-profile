8bda496383da72ca65b636ec3408c24c4b9f3272
Travis Wagner <twagner@globaltrading.local>
2018-09-06 13:23:19 -0400

    partials/_git.sh add some aliases

--- partials/_git.sh
+++ partials/_git.sh
@@ -4,5 +4,10 @@ source ~/bash_profile/partials/_add-repo.sh
 alias git-change='git checkout'
 alias cbranch='git checkout'
 alias st="git status"
-alias commit='git commit'
+alias checkout='git checkout'
+alias status='git status'
+alias gti='git'
 gcaa() { git add --all && git commit -m "$*" ; } # add and commit all files
+
+commit() { git commit -m "$*"  ; }
+acommit() { git add $1 && git commit -m "$*" ; }
