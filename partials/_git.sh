b34306e5b1fd71de80c2bbb1766c687de0ce2d1a
trav <trvswgnr@gmail.com>
2019-01-15 15:06:00 -0500

    add git-prune function to delete branches that dont exist on remote

--- partials/_git.sh
+++ partials/_git.sh
@@ -7,3 +7,9 @@ alias cbranch='git checkout'
 alias st="git status"
 alias commit='git commit'
 gcaa() { git add --all && git commit -m "$*" ; } # add and commit all files
+git-prune() {
+	X=${1:-origin}
+	git remote prune $X
+	# delete local branches to match remote
+	git branch -vv | grep "$X/.*: gone]" | awk '{print $1}' | xargs git branch -d
+}
