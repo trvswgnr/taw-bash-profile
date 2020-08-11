20286914e9139cf818f8689e4ea8d89acf9418f1
trav <trvswgnr@gmail.com>
2020-08-11 12:13:45 -0400

    path changes

--- partials/_base.sh
+++ partials/_base.sh
@@ -7,9 +7,15 @@ export BLOCKSIZE=1k
 # add Homebrew, Go, and MAMP packages at start of path
 export PATH="~/go/bin/:/usr/local/Cellar/:/usr/local/opt/python/libexec/bin:$PATH"
 
-export PATH="/Applications/MAMP/bin/php/php7.3.8/bin:$PATH"
+# use the PHP version that's being used in MAMP
+PHPVER=$(/usr/libexec/PlistBuddy -c "print phpVersion" ~/Library/Preferences/de.appsolute.mamppro.plist)
+export PATH="/Applications/MAMP/bin/php/php${PHPVER}/bin:$PATH"
 
-export PATH="/Application/MAMP/Library/bin:$PATH"
+export PATH="/Applications/MAMP/Library/bin:$PATH"
+
+export PATH="/Users/trav/Library/Python/3.7/bin:$PATH"
+
+export PATH="/Users/trav/miniconda/bin:$PATH"
 
 # set default editor
 # export EDITOR="code"
